4e54f6f48ed5cef35fef68da78b964a3f88054cc
Francesco <francesco@unimore.it>
2019-09-18 12:02:09 +0200

    ok parse EF_Dati_Personali

--- tessera_sanitaria.rb
+++ tessera_sanitaria.rb
@@ -2,8 +2,13 @@
 
 require 'smartcard'
 
+#FIELDS = %w{emettitore data_emissione data_scadenza cognome nome data_nascita
+#sesso statura cod_fiscale comune_emissione comune via}
+FIELDS = %w{issuer issue_data expiration_date surname given_name date_of_birth sex heigth tax_payer_number
+city_of_issue city_of_residence street}
+
 EF_ID_CARTA = "10001003"
-EF_DATI_PERSONALI_LENGTH = 6
+EF_DATI_PERSONALI_LENGTH = 12 # 12 bytes: 6 chars after decoding
 
 SELECT_APDU = %w{00 A4 08 00}
 READ_BINARY_APDU = %w{00 B0 00 00} # when offset is 0
@@ -31,6 +36,24 @@ def compose_read_apdu(length)
   pack_apdu(READ_BINARY_APDU + [ sprintf("%02x", length) ])
 end
 
+def ascii_convert(data)
+  (data.scan(/../)).map {|c| c.to_i(16).chr}.join
+end
+
+# skip PAYLOAD_LENGTH_FIELD_SIZE bytes because it's no actual payload
+def parse(data)
+  do_parse( ascii_convert(data[EF_DATI_PERSONALI_LENGTH..-1]), {}, 0)
+end
+
+# data -> data yet to process; result -> result already calculated as a hash; index -> current index of FIELDS
+def do_parse(data, result, index)
+  return result if ( data.empty? or index >= FIELDS.size )
+  length = data[0...2].to_i(16)
+  payload = data[2...(length + 2)]
+  tail = data[(length + 2)..-1]
+  result[FIELDS[index]] = payload
+  do_parse(tail, result, index + 1)
+end
 
 context = Smartcard::PCSC::Context.new(:system)
 readers = context.readers
@@ -52,14 +75,14 @@ while ( raw_length = card.transmit(compose_read_apdu(EF_DATI_PERSONALI_LENGTH)).
 end
 
 puts "EF_Dati_Personali raw_length: #{raw_length}" if $DEBUG
-ef_dati_personali_payload_length = (raw_length[0..(EF_DATI_PERSONALI_LENGTH * 2)].scan /../).map {|c| c.to_i(16).chr}.join.to_i(16)
+ef_dati_personali_payload_length = ascii_convert(raw_length[0..EF_DATI_PERSONALI_LENGTH]).to_i(16)
 
 while ( raw_payload = card.transmit(compose_read_apdu(ef_dati_personali_payload_length)).unpack('H*').first ) !~ /#{OK_RESPONSE_CODE}$/
   puts "ask for raw_length res: #{raw_length}" if $DEBUG
   sleep TIMEOUT
 end
 puts raw_payload if $DEBUG
-
+puts parse(raw_payload)
 
 # Disconnect
 card.disconnect
